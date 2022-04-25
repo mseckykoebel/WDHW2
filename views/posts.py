fd6bf92502c539e4a2fa9eff717766752d423f08
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-04-24 19:05:54 -0500

    added heroku postgres, and about to add the CLI integration for hosting

--- views/posts.py
+++ views/posts.py
@@ -5,11 +5,12 @@ from views import get_authorized_user_ids
 
 import json
 
+
 def get_path():
-    return request.host_url + 'api/posts/'
+    return request.host_url + "api/posts/"
+
 
 class PostListEndpoint(Resource):
-
     def __init__(self, current_user):
         self.current_user = current_user
 
@@ -19,41 +20,41 @@ class PostListEndpoint(Resource):
         return Response(json.dumps([]), mimetype="application/json", status=200)
 
     def post(self):
-        # create a new post based on the data posted in the body 
+        # create a new post based on the data posted in the body
         body = request.get_json()
-        print(body)  
+        print(body)
         return Response(json.dumps({}), mimetype="application/json", status=201)
-        
-class PostDetailEndpoint(Resource):
 
+
+class PostDetailEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
-        
 
     def patch(self, id):
-        # update post based on the data posted in the body 
+        # update post based on the data posted in the body
         body = request.get_json()
-        print(body)       
+        print(body)
         return Response(json.dumps({}), mimetype="application/json", status=200)
-
 
     def delete(self, id):
         # delete post where "id"=id
         return Response(json.dumps({}), mimetype="application/json", status=200)
 
-
     def get(self, id):
         # get the post based on the id
         return Response(json.dumps({}), mimetype="application/json", status=200)
 
+
 def initialize_routes(api):
     api.add_resource(
-        PostListEndpoint, 
-        '/api/posts', '/api/posts/', 
-        resource_class_kwargs={'current_user': api.app.current_user}
+        PostListEndpoint,
+        "/api/posts",
+        "/api/posts/",
+        resource_class_kwargs={"current_user": api.app.current_user},
     )
     api.add_resource(
-        PostDetailEndpoint, 
-        '/api/posts/<int:id>', '/api/posts/<int:id>/',
-        resource_class_kwargs={'current_user': api.app.current_user}
+        PostDetailEndpoint,
+        "/api/posts/<int:id>",
+        "/api/posts/<int:id>/",
+        resource_class_kwargs={"current_user": api.app.current_user},
     )
