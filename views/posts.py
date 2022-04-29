8db21fba22150bfedaf4920cbbe98cf55eb71a59
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-04-29 11:25:41 -0500

    fixed tests up until post

--- views/posts.py
+++ views/posts.py
@@ -1,6 +1,6 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Post, db
+from models import Post, db, Following
 from views import get_authorized_user_ids
 
 import json
@@ -14,16 +14,69 @@ class PostListEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
 
-    def get(self):
-        # get posts created by one of these users:
-        # print(get_authorized_user_ids(self.current_user))
-        return Response(json.dumps([]), mimetype="application/json", status=200)
+    def get(self):  # HTTP GET
+        args = request.args
+        print(args)
+        # Goal: limit to only user #12 (current_user)'s network
+        #   - oneself
+        #   - ppl #12 are following
 
-    def post(self):
+        # 1. Query the following table to get the user_ids that #12 is following:
+        user_ids_tuples = (
+            db.session.query(Following.following_id)
+            .filter(Following.user_id == self.current_user.id)
+            .order_by(Following.following_id)
+            .all()
+        )
+        print(user_ids_tuples)
+        user_ids = [id for (id,) in user_ids_tuples]
+        print(user_ids)
+        user_ids.append(self.current_user.id)
+
+        # alternative method
+        # user_ids = get_authorized_user_ids(self.current_user)
+        try:
+            limit = int(args.get("limit") or 20)  # 20 is the default
+        except:
+            return Response(
+                json.dumps(
+                    {"message": "could not convert to an int - invalid limit parameter"}
+                ),
+                mimetype="application/json",
+                status=400,
+            )
+        if limit > 50:
+            return Response(
+                json.dumps({"message": "too large - invalid limit parameter"}),
+                mimetype="application/json",
+                status=400,
+            )
+        # posts = Post.query.limit(limit).all()
+        posts = Post.query.filter(Post.user_id.in_(user_ids)).limit(limit).all()
+        posts_json = [post.to_dict() for post in posts]
+        return Response(json.dumps(posts_json), mimetype="application/json", status=200)
+
+    def post(self):  # HTTP POST
         # create a new post based on the data posted in the body
         body = request.get_json()
+
+        if not body.get("image_url"):
+            return Response(json.dumps({"message": "image_url is required!"}), mimetype="application/json", status=400)
+
         print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+
+        new_post = Post(
+            image_url=body.get("image_url"),
+            user_id=self.current_user.id,
+            caption=body.get("caption"),
+            alt_text=body.get("alt_text"),
+        )
+
+        # add and then commit the changes to the database
+        db.session.add(new_post)
+        db.session.commit()
+
+        return Response(json.dumps(new_post.to_dict()), mimetype="application/json", status=201)
 
 
 class PostDetailEndpoint(Resource):
@@ -42,7 +95,8 @@ class PostDetailEndpoint(Resource):
 
     def get(self, id):
         # get the post based on the id
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        print(id)
+        return Response(json.dumps({"id": id}), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
