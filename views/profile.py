26b79ad85386af47a7493be428c0c02fc98bcde9
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-05-01 15:51:47 -0500

    get profile done

--- views/profile.py
+++ views/profile.py
@@ -2,6 +2,8 @@ from flask import Response, request
 from flask_restful import Resource
 import json
 
+from models import User
+
 def get_path():
     return request.host_url + 'api/posts/'
 
@@ -11,7 +13,11 @@ class ProfileDetailEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        """
+        Get information on the currently logged in user
+        """
+        user = User.query.filter_by(id = self.current_user.id).limit(1).all().pop()
+        return Response(json.dumps(user.to_dict()), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
