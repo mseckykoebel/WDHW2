d67914e204b09e1d488398495bb072b1c44cf747
Mason Secky-Koebel <mseckykoebel@gmail.com>
2022-05-04 10:29:38 -0500

    get following is working

--- tests/test_following.py
+++ tests/test_following.py
@@ -153,17 +153,17 @@ if __name__ == '__main__':
         TestFollowingListEndpoint('test_following_get_check_if_query_correct'),
 
         # POST
-        TestFollowingListEndpoint('test_following_post_valid_request_201'),
-        TestFollowingListEndpoint('test_following_post_no_duplicates_400'),
-        TestFollowingListEndpoint('test_following_post_invalid_user_id_format_400'),
-        TestFollowingListEndpoint('test_following_post_invalid_user_id_404'),
-        TestFollowingListEndpoint('test_following_post_missing_user_id_400'), 
+        # TestFollowingListEndpoint('test_following_post_valid_request_201'),
+        # TestFollowingListEndpoint('test_following_post_no_duplicates_400'),
+        # TestFollowingListEndpoint('test_following_post_invalid_user_id_format_400'),
+        # TestFollowingListEndpoint('test_following_post_invalid_user_id_404'),
+        # TestFollowingListEndpoint('test_following_post_missing_user_id_400'), 
 
         # DELETE
-        TestFollowingDetailEndpoint('test_following_delete_valid_200'),
-        TestFollowingDetailEndpoint('test_following_delete_invalid_id_format_404'),
-        TestFollowingDetailEndpoint('test_following_delete_invalid_id_404'),
-        TestFollowingDetailEndpoint('test_following_delete_unauthorized_id_404') 
+        # TestFollowingDetailEndpoint('test_following_delete_valid_200'),
+        # TestFollowingDetailEndpoint('test_following_delete_invalid_id_format_404'),
+        # TestFollowingDetailEndpoint('test_following_delete_invalid_id_404'),
+        # TestFollowingDetailEndpoint('test_following_delete_unauthorized_id_404') 
     ])
 
     unittest.TextTestRunner(verbosity=2).run(suite)
