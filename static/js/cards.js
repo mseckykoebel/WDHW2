53814ae8866a5b58af404b9f5fb6d64da9f90e88
mseckykoebel <mseckykoebel@gmail.com>
2022-05-15 11:08:53 -0500

    like logic is completely done

--- static/js/cards.js
+++ static/js/cards.js
@@ -1,12 +1,138 @@
-// assuming that the entire template is loaded before the "Load X more" button is pressed
+/**
+ * @returns {object} promise - a user object
+ */
+const getProfile = () => {
+  const profile = fetch("http://127.0.0.1:5000/api/profile/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .catch((err) => {
+      console.log("Profile not fetched - error", err);
+    });
+
+  return profile;
+};
+
+/**
+ *
+ * @param {*} ev - an event object
+ * @returns {void} - classes are changed
+ *
+ */
+const toggleLike = (ev, postId, likeId) => {
+  console.log(likeId);
+  const elem = ev.currentTarget;
+  if (elem.classList.contains("heart-icon")) {
+    createNewLike(elem, postId);
+  } else {
+    removeLike(elem, postId, elem.getAttribute("data-like-id"));
+  }
+};
+
+const createNewLike = async (elem, postId) => {
+  console.log("liking");
+  const likeData = {
+    post_id: postId,
+  };
+  // post a new like
+  const response = await fetch("/api/posts/likes/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(likeData),
+  });
+  const data_1 = await response.json();
+  console.log(data_1);
+  elem.classList.add("heart-icon-filled");
+  elem.classList.remove("heart-icon");
+  elem.setAttribute("data-like-id", data_1.id);
+  const count =
+    Number(document.querySelector(`.likes-${postId}`).innerHTML.split(" ")[0]) +
+    1;
+  console.log("THIS IS THE COUNT", count);
+  document.querySelector(`.likes-${postId}`).innerHTML =
+    count == 1 ? count + " like" : count + " likes";
+};
+
+const removeLike = (elem, postId, likeId) => {
+  console.log("unliking");
+  console.log("likeID: ", likeId);
+  // post a new like
+  fetch(`/api/posts/likes/${likeId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      elem.classList.remove("heart-icon-filled");
+      elem.classList.add("heart-icon");
+      const count =
+        Number(
+          document.querySelector(`.likes-${postId}`).innerHTML.split(" ")[0]
+        ) - 1;
+      console.log("THIS IS THE COUNT", count);
+      document.querySelector(`.likes-${postId}`).innerHTML =
+        count == 1 ? count + " like" : count + " likes";
+    });
+  elem.classList.remove("heart-icon-filled");
+  elem.classList.add("heart-icon");
+  elem.setAttribute("data-like-id", null);
+  console.log("unliking");
+};
+
+/**
+ * @param {*} post - post object
+ * @returns {HTMLElement} - an img element with the right class applied
+ */
+const imageTemplate = (post, userId) => {
+  // true if the current user likes this post
+  const userLikes = post.likes.some((like) => like.user_id == userId);
+  // guaranteed to be only one of these
+  const userLikesThis = post.likes.find((like) => like.user_id == userId);
+  let userLikesId = null;
+  if (userLikesThis != undefined) {
+    userLikesId = userLikesThis.id;
+  }
+  // if current user does not like this post
+  if (!userLikes) {
+    return `
+    <img
+      src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+      id="image-${post.id}"
+      data-like-id=${userLikesId}
+      class="icon heart-icon"
+      alt="Heart icon - select this to like the post"
+      onclick="toggleLike(event, ${post.id})"
+    />
+    `;
+  } else {
+    return `
+      <img
+        src="https://cdn-icons-png.flaticon.com/128/2107/2107845.png"
+        id="image-${post.id}"
+        data-like-id=${userLikesId}
+        class="icon heart-icon-filled"
+        alt="Heart icon - select this to like the post"
+        onclick="toggleLike(event, ${post.id})"
+      />
+    `;
+  }
+};
 
 const commentTemplate = (comment) => {
   return `
-    <p class="comment">
-      <span>${comment.user.username}</span>
-      ${comment.text}
-    </p>
-  `;
+      <p class="comment">
+        <span>${comment.user.username}</span>
+        ${comment.text}
+      </p>
+    `;
 };
 
 const commentsTemplate = (post) => {
@@ -22,32 +148,37 @@ const commentsTemplate = (post) => {
   if (comments != undefined && comments.length > 1) {
     // case where there are a lot of comments
     return `
-    <div id="comment_section">
-      ${html}
-      <!-- Button for commenting-->
-      <a class="load-more" href="#"
-        >Load ${post.comments.length - 1} more</a
-      >
-      <p class="post-time">${post.display_time}</p>
-    </div>
-    `;
+      <div id="comment_section">
+        ${html}
+        <!-- Button for commenting-->
+        <a class="load-more" href="#"
+          >Load ${post.comments.length - 1} more</a
+        >
+        <p class="post-time">${post.display_time}</p>
+      </div>
+      `;
     // case where there is exactly one comment
   } else if (comments != undefined && comments.length == 1) {
     return `
-    <div id="comment_section">
-      <p class="comment">
-        ${html}
-      </p>
-      <p class="post-time">${post.display_time}</p>
-    </div>
-    `;
+      <div id="comment_section">
+        <p class="comment">
+          ${html}
+        </p>
+        <p class="post-time">${post.display_time}</p>
+      </div>
+      `;
   } else {
     // case where there are no comments
     return ``;
   }
 };
 
+// assuming that the entire template is loaded before the "Load X more" button is pressed
 const cardsToHtml = (post) => {
+  const profile = document
+    .querySelector(".cards-container")
+    .getAttribute("data-user-id");
+  const heartImage = imageTemplate(post, profile);
   const comments = commentsTemplate(post);
   return `
   <div class="post">
@@ -75,11 +206,7 @@ const cardsToHtml = (post) => {
     />
     <div class="post-content">
       <div class="reaction-wrapper">
-        <img
-          src="https://img.icons8.com/ios-glyphs/344/like--v2.png"
-          class="icon"
-          alt="Heart icon - select this to like the post"
-        />
+        ${heartImage}
         <img
           src="https://img.icons8.com/ios/344/topic.png"
           class="icon"
@@ -96,7 +223,7 @@ const cardsToHtml = (post) => {
           alt="Save icon - select this to save this post to your bookmarks"
         />
       </div>
-      <p class="likes">${post.likes.length} likes</p>
+      <p class="likes data-like-count=${post.likes.length} likes-${post.id}">${post.likes.length} likes</p>
       <p class="description">
         <span>${post.user.username}</span> ${post.caption}
       </p>
